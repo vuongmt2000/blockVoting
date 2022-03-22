f6ba34a558e9460e97290b55292bc6a2610da548
test <vuongmt2000117@email.com>
2022-03-22 10:44:34 +0700

    first commit

---
+++ src/Components/NewPoll.js
@@ -0,0 +1,94 @@
+import React, { useRef, useState } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+
+const NewPoll = (props) => {
+  const candidateName1 = useRef(null);
+  const candidateName2 = useRef(null);
+
+  const candidateName1URL = useRef(null);
+  const candidateName2URL = useRef(null);
+
+  const promptRef = useRef(null);
+
+  const [disableButton, changeDisable] = useState(false);
+
+  const sendToBlockChain = async () => {
+    if(candidateName1URL && candidateName2URL && candidateName1 && candidateName2 &&promptRef){
+      changeDisable(true);
+    await window.contract.addUrl({
+      name: candidateName1.current.value,
+      url: candidateName1URL.current.value,
+    });
+    
+    await window.contract.addUrl({
+      name: candidateName2.current.value,
+      url: candidateName2URL.current.value,
+    });
+
+    await window.contract.addCandidatePair({
+      prompt: promptRef.current.value,
+      name1: candidateName1.current.value,
+      name2: candidateName2.current.value,
+    });
+
+    await window.contract.addToPromptArray({ prompt: promptRef.current.value });
+
+    alert("head back to home page");
+  } else {
+    alert("check entered information ")
+  }
+  };
+
+  return (
+    <Container style={{ marginTop: "10px" }}>
+      <Form>
+        <Form.Group className='mb-3'>
+          <Form.Label>Candidiate 1 Name</Form.Label>
+          <Form.Control
+            ref={candidateName1}
+            placeholder='Enter Candidate Name'
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Candidate 1 Image URL</Form.Label>
+          <Form.Control
+            ref={candidateName1URL}
+            placeholder='enter Image URL'
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Candidiate 2 Name</Form.Label>
+          <Form.Control
+            ref={candidateName2}
+            placeholder='Enter Candidate Name'
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Candidate 2 Image URL</Form.Label>
+          <Form.Control
+            ref={candidateName2URL}
+            placeholder='enter Image URL'
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Prompt</Form.Label>
+          <Form.Control ref={promptRef} placeholder='Add Prompt'></Form.Control>
+        </Form.Group>
+      </Form>
+
+      <Button
+        disabled={disableButton}
+        onClick={sendToBlockChain}
+        variant='primary'
+      >
+        Submit
+      </Button>
+    </Container>
+  );
+};
+
+export default NewPoll;
